17b0cc59eed15c74029627a0414d4bc46e9bbc7c
Gabriel Dzul <gabrieldzul@gmail.com>
2020-09-26 11:37:11 -0500

    Initial commit

---
+++ lib/banxico/sie.rb
@@ -0,0 +1,8 @@
+require "banxico/sie/version"
+
+module Banxico
+  module Sie
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/banxico/sie/version.rb
@@ -0,0 +1,5 @@
+module Banxico
+  module Sie
+    VERSION = "0.1.0"
+  end
+end
