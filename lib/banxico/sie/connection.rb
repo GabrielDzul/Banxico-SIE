38c7169bf52ac7cce661ae5289742ca3403a8554
Gabriel Dzul <gabrieldzul@gmail.com>
2020-09-28 11:39:14 -0500

    Added connection object

---
+++ lib/banxico/sie/connection.rb
@@ -0,0 +1,13 @@
+module Banxico
+  module SIE
+    def self.connection
+      Faraday.new(url: Banxico::SIE.base_uri ) do |faraday|
+
+        # faraday.response :logger
+        faraday.adapter Faraday.default_adapter
+        faraday.use Faraday::Response::RaiseError
+        faraday.headers { 'Bmx-Token': Banxico::SIE.key }
+      end
+    end
+  end
+end
