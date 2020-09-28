5c83cb71f0344a7a6bde52c14dcb23bcd1572bd6
Gabriel Dzul <gabrieldzul@gmail.com>
2020-09-28 15:53:26 -0500

    Added serie code

--- lib/banxico/sie/endpoints.rb
+++ lib/banxico/sie/endpoints.rb
@@ -1,7 +1,9 @@
 module Banxico
   module SIE
     module Endpoints
-      USD_EXCHANGE_RATE = 'series/SF43718/datos/oportuno'
+      USD_EXCHANGE_CODE = "SF43718"
+
+      USD_EXCHANGE_RATE = "series/#{USD_EXCHANGE_CODE}/datos/oportuno"
     end
   end
 end
