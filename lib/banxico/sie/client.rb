71689ddfe1aa29a087d666b105d47549aaba815f
Gabriel Dzul <gabrieldzul@gmail.com>
2020-09-28 15:53:57 -0500

    Fixed params

--- lib/banxico/sie/client.rb
+++ lib/banxico/sie/client.rb
@@ -1,7 +1,7 @@
 module Banxico
   module SIE
     module Client 
-      def self.get(path:, body:, headers: {})
+      def self.get(path:, body: nil, headers: {})
         response = Banxico::SIE.connection.get path, body, headers
         JSON.parse(response.body)
       rescue Faraday::ConnectionFailed, Faraday::TimeoutError
