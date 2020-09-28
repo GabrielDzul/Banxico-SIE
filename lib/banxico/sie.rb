8f058893d86bcce53b28bb9b57a20879dbd53a67
Gabriel Dzul <gabrieldzul@gmail.com>
2020-09-28 12:04:21 -0500

    Added usd exchange rate service

--- lib/banxico/sie.rb
+++ lib/banxico/sie.rb
@@ -1,8 +1,33 @@
 require "banxico/sie/version"
+require "banxico/sie/connection"
+require "banxico/sie/endpoints"
+require "banxico/sie/usd_exchange_rate"
+require "banxico/sie/errors/network_error"
 
 module Banxico
-  module Sie
-    class Error < StandardError; end
-    # Your code goes here...
+  module SIE
+    def self.config
+      yield self
+    end
+  
+    def self.version
+      @version
+    end
+  
+    def self.base_uri
+      @base_uri
+    end
+  
+    def self.base_uri=(base_uri)
+      @base_uri = base_uri
+    end
+  
+    def self.key
+      @key
+    end
+  
+    def self.key=(key)
+      @key = key
+    end
   end
 end
